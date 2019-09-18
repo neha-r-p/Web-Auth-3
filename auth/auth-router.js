b06c8db627f38949ab911869e6f1f52b3d464bed
Neha Patel <neha.r.p@gmail.com>
2019-09-18 15:07:58 -0700

    register route working

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,23 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
+const Users = require("../users/users-model");
 
-module.exports = router
+module.exports = router;
+
+router.post("/register", (req, res) => {
+  let newUser = req.body;
+  const hash = bcrypt.hashSync(newUser.password, 12);
+  newUser.password = hash;
+
+  Users.add(newUser)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: "Error creating new user" });
+    });
+});
