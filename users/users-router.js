aa06daee90df7fc08e9887e557c02eca79cf2c73
Neha Patel <neha.r.p@gmail.com>
2019-09-18 15:24:49 -0700

    GET users request working, need to add restricted middleware

--- users/users-router.js
+++ users/users-router.js
@@ -2,4 +2,17 @@ const express = require("express");
 
 const router = express.Router();
 
-module.exports = router
+const Users = require("./users-model.js");
+
+router.get("/", (req, res) => {
+  Users.find()
+    .then(users => {
+        res.status(200).json(users)
+    })
+    .catch(err => {
+      console.log("GET error", err);
+      res.status(500).json({ message: "error retrieving users" });
+    });
+});
+
+module.exports = router;
