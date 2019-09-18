c49af0ec965fceeef32f3b685a508ee6eb3742e9
Neha Patel <neha.r.p@gmail.com>
2019-09-18 15:15:29 -0700

    created login endpoint, and generateToken function. need to create secret before testing

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -21,3 +21,18 @@ router.post("/register", (req, res) => {
       res.status(500).json({ error: "Error creating new user" });
     });
 });
+
+router.post("/login", (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ token });
+      } else {
+          res.status(401).json({ error: "You shall not pass!"})
+      }
+    });
+});
