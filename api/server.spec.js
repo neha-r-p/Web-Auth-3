c83a44ac328e73aadfd6b32c8daf56896217a562
Neha Patel <neha.r.p@gmail.com>
2019-09-19 14:03:38 -0700

    testing complete for GET / route

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,14 +1,21 @@
 const request = require("supertest");
 
-const server = require('./server.js')
+const server = require("./server.js");
 
-describe('server.js', () => {
-    describe('index route', () => {
-        it('should return status 200 from index route', async () => {
-            const expectedStatus = 200;
+describe("server.js", () => {
+  describe("index route", () => {
+    it("should return status 200 from index route", async () => {
+      const expectedStatus = 200;
 
-            const response = await request(server).get('/');
-            expect(response.status).toEqual(expectedStatus)
-        })
-    })
-})
+      const response = await request(server).get("/");
+      expect(response.status).toEqual(expectedStatus);
+    });
+
+    it("should return a JSON object", async () => {
+      const expectedBody = { api: "running" };
+
+      const response = await request(server).get("/");
+      expect(response.body).toEqual(expectedBody);
+    });
+  });
+});
