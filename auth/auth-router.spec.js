43abb61a79d1603a9e85371e95a777099eccc2a9
Neha Patel <neha.r.p@gmail.com>
2019-09-19 14:37:42 -0700

    test to add user works

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -1,13 +1,24 @@
-
 const Users = require("../users/users-model");
 const db = require("../data/db-config.js");
 
-describe('auth router', () => {
-    beforeEach( async () => {
-        await db('users').truncate();
-    })
+describe("auth router", () => {
+  beforeEach(async () => {
+    await db("users").truncate();
+  });
 
-    it('should set environment to testing', () => {
-        expect(process.env.DB_ENV).toBe('testing')
-    })
-})
+  it("should set environment to testing", () => {
+    expect(process.env.DB_ENV).toBe("testing");
+  });
+
+  describe('add()', () => {
+      it('should add user into the db', async () => {
+          await Users.add({ username: 'bum', password: 'butts', department: 'behind' })
+
+          let users = await db('users');
+
+          expect(users).toHaveLength(1);
+      })
+  })
+
+
+});
